51e9ccc4f905899844f18ad069ece69b2f33ed7e
mRahulJain <rj.jain.me@gmail.com>
2021-04-08 18:04:45 +0530

    phase 03 - assessment update Product.java

--- Phase03/Springboot-SportyShoes/src/main/java/com/springboot/pojo/Product.java
+++ Phase03/Springboot-SportyShoes/src/main/java/com/springboot/pojo/Product.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Product {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "product_id")
 	private int id;
 	
